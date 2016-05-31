b6862f5f536fd66696ece03fa555e1a4d289a9b1
32BitStew <stewart@Stewarts-iMac.local>
2016-05-30 18:13:22 -0700

    Added customizable palettes
    I added a few default palettes to the GBPalettes class and an array
    constructor so that a palette can be made from any array of four colors.
    
    Note that using these would not simply affect a palette swap on the
    normal result… Because the colors that the Gameboyifier compares per
    pixel are totally different, the image becomes totally different too!

--- GBfier/GBPalette.java
+++ GBfier/GBPalette.java
@@ -1,10 +1,18 @@
 import java.awt.Color;
 
 public class GBPalette {
+  private static final Color[] ORIGINAL = {new Color(15,42,14), new Color(39,80,38), new Color(140,170,38), new Color(156,186,41)};
+  private static final Color[] POCKET = {Color.BLACK, new Color(82,82,82), new Color(148,148,148), Color.WHITE};
+  private static final Color[] AWAKENING = {new Color(6,24,31), new Color(50,102,81), new Color(137,191,115), new Color(224,247,210)};
+
   private static Color[] palette;
 
   public GBPalette() {
-    this(new Color(15,42,14), new Color(39,80,38), new Color(140,170,38), new Color(156,186,41));
+    this(ORIGINAL);
+  }
+
+  public GBPalette(Color[] colors) {
+    if(colors.length == 4) this.palette = colors;
   }
 
   public GBPalette(Color c0, Color c1, Color c2, Color c3) {
